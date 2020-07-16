ad63221507bbef4aaf7e7237e0259cfdea73cf0f
Nadav Daester <daesternadav@gmail.com>
2020-07-16 17:14:02 +0300

    try

--- src/components/header.cmp.vue
+++ src/components/header.cmp.vue
@@ -5,7 +5,7 @@
       <span class="logo-name">RCRentals</span>
     </router-link>
     <div class="flex nav">
-      <p>List your car+</p>
+      <p>List your car-</p>
       <p>about</p>
       <p>Log in</p>
       <p>Sign up</p>
