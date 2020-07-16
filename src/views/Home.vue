df2a3cde24b4d91791dba0545accba1aca22f86c
Nadav Daester <daesternadav@gmail.com>
2020-07-16 19:34:56 +0300

    home page update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <section>
     <div class="bgc-img">
-      <div class="text-img" >
+      <div class="text-img">
         <h1>Way better than a rental car</h1>
         <h3>Book unforgettable cars from trusted hosts around the world</h3>
         <input type="text" name id />
       </div>
     </div>
-
     <h2>Browse by category</h2>
-    <div>img img img img</div>
+    <div class="browse-category flex space-between">
+      <div>
+        <img src="../assets/hero3.jpg" height="120" />
+        <h3>Vintage</h3>
+      </div>
+      <div>
+        <img src="../assets/hero3.jpg" height="120" />
+        <h3>Luxury</h3>
+      </div>
+      <div>
+        <img src="../assets/hero3.jpg" height="120" />
+        <h3>Sports</h3>
+      </div>
+    </div>
 
     <h2>you might like</h2>
-    <div>img img img img</div>
+    <div class="flex space-between">
+      <div>
+        <img src="../assets/hero3.jpg" height="300" />
+        <h3>Vintage</h3>
+      </div>
+      <div>
+        <img src="../assets/hero3.jpg" height="300" />
+        <h3>Luxury</h3>
+      </div>
+      <div>
+        <img src="../assets/hero3.jpg" height="300" />
+        <h3>Sports</h3>
+      </div>
+    </div>
 
     <h3>Discover the world’s largest car sharing marketplace</h3>
   </section>
