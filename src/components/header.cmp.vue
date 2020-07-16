3fea127a652b96c7c1216e80deda08613860bd32
Maor <maorba6@gmail.com>
2020-07-16 17:09:16 +0300

    zxczxc

--- src/components/header.cmp.vue
+++ src/components/header.cmp.vue
@@ -2,11 +2,12 @@
   <section class="app-header flex" >
 
 
+
       <router-link to="/"> <img class="header-logo-img" src="@/assets/logo.png"> RCRentals</router-link>
       <div class="flex">
         <router-link to="/about">About</router-link>  
           <p>list your car</p>
-          <p>learn more</p>
+          <p>learn moreR</p>
           <p>log in</p>
           <p>sign up</p>
           <p>user img || default img</p>
